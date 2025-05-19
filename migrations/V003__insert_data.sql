5310525c9c4e76577fd9c964b56d5120709f5fed
ZeraiGR <kirillgr15@mail.ru>
2025-05-19 21:27:08 +0000

    upd migrations with splitted to chunks

--- migrations/V003__insert_data.sql
+++ migrations/V003__insert_data.sql
@@ -6,16 +6,33 @@ INSERT INTO product (id, name, picture_url, price) VALUES
 (5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00),
 (6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
 
-INSERT INTO orders (id, status, date_created)
-SELECT 
-    i, 
-    (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], 
-    DATE(NOW() - (random() * (NOW()+'90 days' - NOW())))
-FROM generate_series(1, 10000000) s(i);
+DO $$
+BEGIN
+  FOR i IN 1..100 LOOP
+    INSERT INTO orders (id, status, date_created)
+    SELECT 
+      (i-1)*100000 + row_number() OVER (),
+      (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], 
+      DATE(NOW() - (random() * (NOW()+'90 days' - NOW())))
+    FROM generate_series(1, 100000) s;
+    
+    RAISE NOTICE 'Inserted orders batch %', i;
+    COMMIT;
+  END LOOP;
+END $$;
 
-INSERT INTO order_product (quantity, order_id, product_id)
-SELECT
-    floor(1+random()*50)::int,
-    i,
-    1 + floor(random()*6)::int % 6
-FROM generate_series(1, 10000000) s(i);
+DO $$
+BEGIN
+  FOR i IN 1..100 LOOP
+    INSERT INTO order_product (quantity, order_id, product_id)
+    SELECT 
+      floor(1+random()*50)::int, 
+      (i-1)*100000 + row_number() OVER (),
+      1 + floor(random()*6)::int % 6
+    FROM generate_series(1, 100000) s;
+    
+    RAISE NOTICE 'Inserted order_product batch %', i;
+    COMMIT;
+  END LOOP;
+END $$;
+
